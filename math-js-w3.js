905e194df4895a7ff766449d220ca2bfb1f1d644
brandanblevins <brandan.blevins@gmail.com>
2022-12-08 14:14:17 -0700

    Add more math examples

--- math-js-w3.js
+++ math-js-w3.js
@@ -12,3 +12,17 @@ console.log(y);
 let c = 500 + 250 * 4;
 
 console.log(c);
+
+let d = 123e5; // Scientific notation
+
+console.log(d);
+
+let e = x + y;
+
+console.log(e); // Adding two numbers results in a number.
+
+let f = "20";
+
+let g = e + f;
+
+console.log(g); // Adding a string plus a number results in a string concatenation.
