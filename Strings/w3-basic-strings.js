dbb35b7c53ce3b6821c8d4aed2079abb362520aa
brandanblevins <brandan.blevins@gmail.com>
2022-12-09 10:54:28 -0700

    Test slice method for strings

--- Strings/w3-basic-strings.js
+++ Strings/w3-basic-strings.js
@@ -2,3 +2,8 @@ let text = "sdflajfksdjlaflkfjlksalfjdkdasfdsaf";
 let length = text.length;
 
 console.log(length);
+
+let text2 = "Apple, Banana, Pear, Orange";
+let part = text2.slice(5, 17);
+
+console.log(part);
