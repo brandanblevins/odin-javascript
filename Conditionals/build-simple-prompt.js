c1b2d0383957b9b8000a9b6aa3a9eea6d9397b58
brandanblevins <brandan.blevins@gmail.com>
2022-12-11 10:24:14 -0700

    Add note that explains prompt

--- Conditionals/build-simple-prompt.js
+++ Conditionals/build-simple-prompt.js
@@ -9,6 +9,14 @@ if (age > 18) {
 
 alert(accessAllowed);
 
+
+/* It may be difficult at first to grasp what’s going on. But after a closer look, we can see that it’s just an ordinary sequence of tests:
+
+    The first question mark checks whether age < 3.
+    If true – it returns 'Hi, baby!'. Otherwise, it continues to the expression after the colon “:”, checking age < 18.
+    If that’s true – it returns 'Hello!'. Otherwise, it continues to the expression after the next colon “:”, checking age < 100.
+    If that’s true – it returns 'Greetings!'. Otherwise, it continues to the expression after the last colon “:”, returning 'What an unusual age!'. */
+
 let age2 = prompt('age?', 18);
 
 let message = (age2 < 3) ? 'Hi, baby!' :
