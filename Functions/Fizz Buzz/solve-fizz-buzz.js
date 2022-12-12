43960fd0d36a55fb155cffb1f8c429b98c837563
brandanblevins <brandan.blevins@gmail.com>
2022-12-12 13:15:02 -0700

    Add step 5 of Fizz Buzz

--- Functions/Fizz Buzz/solve-fizz-buzz.js
+++ Functions/Fizz Buzz/solve-fizz-buzz.js
@@ -1,11 +1,13 @@
 let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "))
 
 for (let i = 1; i <= answer; i++) {
-    if (i % 3 === 0) {
-        alert("Fizz");
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
     } else if (i % 5 === 0) {
-        alert("Buzz");
+        console.log("Buzz");
     } else {
-        alert(i);
+        console.log(i);
     }
 }
