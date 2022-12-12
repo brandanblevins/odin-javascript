7611ad05c2a0e8f5e7d34d6fa2072f0bf4fe3546
brandanblevins <brandan.blevins@gmail.com>
2022-12-12 10:11:59 -0700

    Add example that shows a local variable to function

--- Functions/function-basics.js
+++ Functions/function-basics.js
@@ -9,3 +9,13 @@ function showMessage() {
 
 showMessage();
 showMessage();
+
+function showMessage2() {
+  let message = "Hello, I'm JavaScript!"; // local variable
+
+  console.log( message );
+}
+
+showMessage2(); // Hello, I'm JavaScript!
+
+console.log( message ); // <-- Error! The variable is local to the function
